ab0288be823fbc5468bfa67beb0fddb9a8626582
Yam <haoshaochun@gmail.com>
2018-03-07 15:42:37 +0800

    update writefiles

--- BasicTools/writefiles/writefiles.py
+++ BasicTools/writefiles/writefiles.py
@@ -1,12 +1,15 @@
 import jieba
 
 
-def write2txt(file, datalist, MIN, MAX, listlen=True):
+def write2txt(file, datalist, lenneed=True, MIN=8, MAX=32, listlen=True):
     with open(file, 'w') as f:
         for line in datalist:
-            if listlen:
-                lenline = len(jieba.lcut(line))
+            if lenneed:
+                if listlen:
+                    lenline = len(jieba.lcut(line))
+                else:
+                    lenline = len(line)
+                if lenline >= MIN and lenline <= MAX:
+                    f.write(line+"\n")
             else:
-                lenline = len(line)
-            if lenline >= MIN and lenline <= MAX:
                 f.write(line+"\n")
