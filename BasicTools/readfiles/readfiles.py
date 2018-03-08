6768b2a5e4df73c5baac878ff40c0fdc18d511a4
Yam <haoshaochun@gmail.com>
2018-03-08 09:47:54 +0800

    update readfiles

--- BasicTools/readfiles/readfiles.py
+++ BasicTools/readfiles/readfiles.py
@@ -6,20 +6,21 @@ ENGLISH = 'E'
 pnum = re.compile(rf'[１２３４５６７８９０\d]+')
 peng = re.compile(rf'[a-zA-Z]+')
 p_pairpuc = re.compile(r'''
-                       (?P<p_pairpuc>
-                       【[\w\s\d]+】
-                       |〖[\w\s\d]+〗
-                       |〈[\w\s\d]+〉
-                       |＜[\w\s\d]+＞
-                       |<[\w\s\d]+>
-                       |〔[\w\s\d]+〕
-                       |﹝[\w\s\d]+﹞
-                       |［[\w\s\d]+］
-                       |\[[\w\s\d]+\]
-                       |（[\w\s\d]+）
-                       |\([\w\s\d]+\)
-                       |\{[\w\s\d]+\}
-                       )''', re.UNICODE | re.VERBOSE) # 标准对
+    (?P<p_pairpuc>
+    【[\w\s\d]+】
+    |〖[\w\s\d]+〗
+    |〈[\w\s\d]+〉
+    |＜[\w\s\d]+＞
+    |<[\w\s\d]+>
+    |〔[\w\s\d]+〕
+    |﹝[\w\s\d]+﹞
+    |［[\w\s\d]+］
+    |\[[\w\s\d]+\]
+    |（[\w\s\d]+）
+    |\([\w\s\d]+\)
+    |\{[\w\s\d]+\}
+    )''', re.UNICODE | re.VERBOSE)  # 标准对
+
 
 def readtext_raw(file, m=0):
     data = []
@@ -33,6 +34,7 @@ def readtext_raw(file, m=0):
                 break
     return data
 
+
 def readtext_clean(file):
     data = []
     with open(file, 'r') as f:
@@ -41,12 +43,14 @@ def readtext_clean(file):
             line = peng.sub(ENGLISH, line)
             line = pnum.sub(DIGIT, line)
             line = line.replace(" ", "")
-            line = line.replace("　　","")
-            line = line.replace(u"\u3000","")
+            line = line.replace("　　", "")
+            line = line.replace(u"\u3000", "")
             line = line.replace("“", "\"")
             line = line.replace("”", "\"")
             line = line.replace("……", "…")
             line = line.replace("…", "……")
             line = p_pairpuc.sub("", line)
+            if len(line) == 0:
+                continue
             data.append(line)
     return data
