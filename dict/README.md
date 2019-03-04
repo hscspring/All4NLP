20f0579b2da6c44c040d74669f324a442dcf0a48
Yam <haoshaochun@gmail.com>
2019-03-04 12:04:06 +0800

    update

--- dict/README.md
+++ dict/README.md
@@ -1,5 +1,10 @@
 # YamNLP 常用词表
 
 - 结巴词性对照表
-- 停用词：常用停用词，包含了：百度、哈工大、川大机器智能实验室、中文停用词库以及单独整理的标点。
-- StanfordParserDict：斯坦福句法分析字典
+- stop_words：常用停用词，包含了：百度、哈工大、川大机器智能实验室、中文停用词库以及单独整理的标点。
+- `StanfordParserDict.txt`：斯坦福句法分析字典
+- `chinese_stroke.txt`：汉字笔画字典，来自 [WTree/chineseStroke: 汉字笔画库](https://github.com/WTree/chineseStroke) 和维基百科。
+- `common_words.txt`：常用词，来自 GitHub 的两个仓库，具体地址不记得了。
+- `3500*.text`：3500 常用字 Unicode 和 五笔编码
+- `chinese_idioms.txt`：成语词典
+
--- nbt/readfiles/readfiles.py
+++ nbt/readfiles/readfiles.py
@@ -3,35 +3,54 @@ import re
 
 DIGIT = 'D'
 ENGLISH = 'E'
+# 空白
 pwhi = re.compile(rf'[\t\u3000 ]+')
-pnum = re.compile(rf'[１２３４５６７８９０\d]+[.%:：]*[１２３４５６７８９０\d]*')
+# 数字
+pnum = re.compile(rf'-*[１２３４５６７８９０\d]+[.%:：]*[１２３４５６７８９０\d%]*')
+# 英文
 peng = re.compile(rf'[a-zA-Z]+')
-pnormal = re.compile(rf'[\u2E80-\uFE4Fa-zA-Z\d、，。：；！？…《》（）\(\)『』「」]+')
+# 图片
+ppic = re.compile(r'!\[.*\]\(.+\)')
+# 链接
+plink = re.compile(r'\[\w+\]\(.+\)')
+# 井号星号
+pjxhao = re.compile(r'[>*#\xa0]+')
+# 表情
+pemj = re.compile(
+    u'['u'\U0001F300-\U0001F64F'u'\U0001F680-\U0001F6FF'u'\u2600-\u2B55]+',
+    re.UNICODE)
+# 纯汉字
+pzh = re.compile(rf'[\u4e00-\u9fa5]+')
+# 汉字、英文、数字、常规标点
+pnormal = re.compile(rf'[\u4e00-\u9fa5a-zA-Z\d、，。：；！“”‘’？…《》（）\(\)『』「」\n]+')
+# 中文大字符集
+pzh_big = re.compile(rf'[\u2E80-\uFE4F]+')
 # 替换掉类似（1）【1】这样的序号
 p_pairpuc = re.compile(r'''
     (?P<p_pairpuc>
-    \W【[\s\d]+】\W
-    |\W〖[\s\d]+〗\W
-    |\W〈[\s\d]+〉\W
-    |\W＜[\s\d]+＞\W
-    |\W<[\s\d]+>\W
-    |\W〔[\s\d]+〕\W
-    |\W﹝[\s\d]+﹞\W
-    |\W［[\s\d]+］\W
-    |\W\[[\s\d]+\]\W
-    |\W（[\s\d]+）\W
-    |\W\([\s\d]+\)\W
-    |\W\{[\s\d]+\}\W
+    【[\s\d]+】
+    |〖[\s\d]+〗
+    |〈[\s\d]+〉
+    |＜[\s\d]+＞
+    |<[\s\d]+>
+    |〔[\s\d]+〕
+    |﹝[\s\d]+﹞
+    |［[\s\d]+］
+    |\[[\s\d]+\]
+    |（[\s\d]+）
+    |\([\s\d]+\)
+    |\{[\s\d]+\}
     )''', re.UNICODE | re.VERBOSE)  # 标准对
 # 替换掉类似 1、1. 这样的序号
 p_order = re.compile(rf'[ ]*[\d]+[、，. ]+')
 # 替换掉多余的标点符号
 p_punc = re.compile(rf'[、，。：；！？…]+')
 
+
 def readtext_raw(file, m=0, encoding='utf-8'):
     data = []
     n = 0
-    with open(file, 'r', encoding=encoding) as f:
+    with open(file, 'r', encoding=encoding, errors='ignore') as f:
         for line in f.readlines():
             line = line.strip()
             if len(line) == 0:
@@ -43,31 +62,37 @@ def readtext_raw(file, m=0, encoding='utf-8'):
     return data
 
 
-def readtext_clean(file, reporder=True, repeng=False, repnum=True, normaltext=True):
+def readtext_clean(file,
+                   reporder=False,
+                   repeng=False,
+                   repnum=False,
+                   normaltext=False):
     data = []
     with open(file, 'r') as f:
         for line in f.readlines():
             line = line.strip()
-            line = line.lower()
+            line = re.compile(rf'[\n]+').sub("\n", str(line))
+            line = pwhi.sub(" ", line)
+            line = ppic.sub("", line)
+            line = plink.sub("", line)
+            line = pjxhao.sub("", line)
+            line = pemj.sub("", line)
+            line = line.replace(",", "，")
+            line = line.replace(":", "：")
+            line = line.replace(";", "；")
+            # 点替换为句号，但点后面有数字不替换
+            line = re.compile(r'(?<!\d)[.]').sub("。", line)
+            line = re.compile(rf'[?？]+').sub("？", line)
+            line = re.compile(rf'[!！]+').sub("！", line)
+            line = re.compile(rf'[。]+').sub("。", line)
             if reporder:
                 line = p_pairpuc.sub(" ", line)
-                line = p_order.sub(" ", line)
             if repeng:
                 line = peng.sub(ENGLISH, line)
             if repnum:
                 line = pnum.sub(DIGIT, line)
-            line = pwhi.sub(" ", line)
-            line = line.replace(",", "，")
-            line = line.replace(".", "。")
-            line = line.replace("?", "？")
-            line = line.replace("!", "！")
-            line = line.replace(":", "：")
-            line = line.replace(";", "；")
-            line = line.replace("\"", "“")
-            line = line.replace("”", "“")
-            line = line.replace("……", "…")
             if normaltext:
-                line = "".join(pnormal.findall(line))
+                line = " ".join(pnormal.findall(line))
             if len(line) == 0:
                 continue
             data.append(line)
